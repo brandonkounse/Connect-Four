ca3acdce0aee7920e7fcca4f123314149e4d075f
Brandon Kounse <kounse@gmail.com>
2023-08-17 16:03:51 -0500

    reworked expectation for full? and wrote tests for count method

--- spec/column_spec.rb
+++ spec/column_spec.rb
@@ -36,7 +36,35 @@ describe Column do
     context 'when row is full' do
       it 'returns true' do
         new_column.row.map! { |spot| spot = '⚫' }
-        expect(new_column.full?).to be true
+        expect { new_column.count }.to change { new_column.full? }.to true
+      end
+    end
+  end
+
+  describe 'count' do
+    context 'when three spots are occupied' do
+      subject(:three_column) { described_class.new }
+
+      before do
+        three_column.row.map!.with_index { |spot, index| spot = '⚫' if index.odd? }
+      end
+
+      it 'returns 3' do
+        three = 3
+        expect(three_column.count).to eq(three)
+      end
+    end
+
+    context 'when five spots are occupied' do
+      subject(:five_column) { described_class.new }
+
+      before do
+        five_column.row.map!.with_index { |spot, index| spot = '⚫' unless index.zero? }
+      end
+
+      it 'returns 5' do
+        five = 5
+        expect(five_column.count).to eq(five)
       end
     end
   end
