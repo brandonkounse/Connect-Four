6343edd06797419070d9a9fb27501a434d7fb59b
Brandon Kounse <kounse@gmail.com>
2023-08-17 15:05:21 -0500

    updated Array.new to not point to same object

--- lib/column.rb
+++ lib/column.rb
@@ -5,13 +5,13 @@ class Column
   attr_reader :row, :count
 
   def initialize
-    @row = Array.new(6)
+    @row = Array.new(6) { |i| i = nil }
     @count = 0
     @full = false
   end
 
   def full?
-    @full = true if @row.any? { |spot| !spot.nil? }
+    @full = true if @row.all? { |spot| !spot.nil? }
 
     @full
   end
