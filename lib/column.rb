192761fe172cc5294d8de953aae0db0e93526e88
Brandon Kounse <kounse@gmail.com>
2023-08-17 14:38:21 -0500

    refactored full?

--- lib/column.rb
+++ lib/column.rb
@@ -2,11 +2,17 @@
 
 # Column state passed to Grid class
 class Column
-  attr_reader :row, :count, :full
+  attr_reader :row, :count
 
   def initialize
     @row = Array.new(6)
     @count = 0
     @full = false
+  end
+
+  def full?
+    @full = true if @row.any? { |spot| !spot.nil? }
+
+    @full
   end
 end
