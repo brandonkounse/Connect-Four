5cddedea5a3225797740928c4cae2a7df4e13ce2
Brandon Kounse <kounse@gmail.com>
2023-08-24 19:24:37 -0500

    refactored tests in line with class file refactor

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -9,11 +9,12 @@ describe ConnectFour do
       subject(:connect_four) { described_class.new }
       let(:player_red) { connect_four.player_red }
       let(:grid) { connect_four.grid }
+      let(:last_spot) { 5 }
 
       context 'when the column in not full' do
         it 'drops piece at selected column' do
           allow(player_red).to receive(:input).and_return('7')
-          expect(connect_four.take_turn(player_red)).to eq(:success)
+          expect { connect_four.take_turn(player_red) }.to change { grid.columns[:seventh].squares[last_spot] }.to('🔴')
         end
       end
 
@@ -26,12 +27,12 @@ describe ConnectFour do
 
         it 'fails once, then succeeds when another column is chosen' do
           allow(player_red).to receive(:input).and_return('7', '6')
-          expect(connect_four.take_turn(player_red)).to eq(:success)
+          expect { connect_four.take_turn(player_red) }.to change { grid.columns[:sixth].squares[last_spot] }.to('🔴')
         end
 
         it 'fails three times, then succeeds when another column is chosen' do
           allow(player_red).to receive(:input).and_return('0', '7', '7', '4')
-          expect(connect_four.take_turn(player_red)).to eq(:success)
+          expect { connect_four.take_turn(player_red) }.to change { grid.columns[:fourth].squares[last_spot] }.to('🔴')
         end
       end
     end
