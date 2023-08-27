d1cec4ac0435ba9764db0db0e847ce3f4c032fe1
Brandon Kounse <kounse@gmail.com>
2023-08-27 16:40:04 -0500

    started 2d array refactor and wrote first test for implementation

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,46 +1,21 @@
 # frozen_string_literal: true
 
-require_relative 'column'
-
 # Gameplay area for Connect Four
 class Grid
-  attr_reader :columns
+  EMPTY_SPOT = '⚪'
+  attr_reader :spots
 
   def initialize
-    @columns = {
-      first: Column.new,
-      second: Column.new,
-      third: Column.new,
-      fourth: Column.new,
-      fifth: Column.new,
-      sixth: Column.new,
-      seventh: Column.new
-    }
+    @spots = Array.new(7) { Array.new(6) { EMPTY_SPOT } }
   end
 
-  def update(column, symbol)
-    return :success if column.update(symbol)
-
-    :full
-  end
-
-  def rows
-    queue = []
-    current_index = 0
-    last_index = 6
-    until current_index == last_index
-      @columns.each_pair do |_key, value|
-        queue << value.spots[current_index]
-      end
-      current_index += 1
-    end
-    queue
-  end
-
-  def full?
-    @columns.each_pair.all? do |_key, value|
-      value.count
-      value.full?
+  def drop_piece(column, symbol, index = 5)
+    if @spots[column][index].nil?
+      :full
+    elsif @spots[column][index] == EMPTY_SPOT
+      @spots[column][index] = symbol
+    else
+      drop_piece(column, symbol, index - 1)
     end
   end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,68 +4,14 @@ require_relative 'spec_helper'
 require_relative '../lib/grid'
 
 describe Grid do
-  describe 'initialize' do
-    context 'when a new grid is created' do
-      subject(:new_grid) { described_class.new }
-      let(:columns) { new_grid.columns }
-
-      it 'has a grid width of 7' do
-        grid_width = 7
-        expect(columns.length).to eq(grid_width)
-      end
-
-      it 'has a grid height of 6' do
-        spots = columns[:first].spots
-        grid_height = 6
-        expect(spots.length).to eq(grid_height)
-      end
-    end
-  end
-
-  describe 'update' do
+  describe 'drop_piece' do
     subject(:grid) { described_class.new }
-    let(:column) { grid.columns[:first] }
-    let(:symbol) { '🔴' }
+    let(:column) { 6 }
+    let(:symbol) { '⚫' }
 
-    context 'when a column is successfully updated' do
-      it 'returns :success' do
-        expect(grid.update(column, symbol)).to eq(:success)
-      end
-    end
-
-    context 'when a column fails to update' do
-      before do
-        6.times { grid.update(column, symbol) }
-      end
-
-      it 'returns :full' do
-        expect(grid.update(column, symbol)).to eq(:full)
-      end
-    end
-  end
-
-  describe 'rows' do
-    subject(:grid) { described_class.new }
-
-    context 'when all indices are empty' do
-      it 'returns a queue of 42 ⚪' do
-        expect(grid.rows.count('⚪')).to eq(42)
-      end
-    end
-  end
-
-  describe 'full?' do
-    subject(:full_grid) { described_class.new }
-
-    context 'when grid is full' do
-      before do
-        full_grid.columns.each_pair do |_key, value|
-          value.spots.map! { '⚫' }
-        end
-      end
-
-      it 'returns true' do
-        expect(full_grid.full?).to be true
+    context 'when column is empty' do
+      it 'drops piece to bottom spot' do
+        expect { grid.drop_piece(column, symbol) }.to change { grid.spots[6][5] }.to(symbol)
       end
     end
   end
