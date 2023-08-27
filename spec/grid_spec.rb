b2a14881d0a50609c6d52ac661c40ca167713d62
Brandon Kounse <kounse@gmail.com>
2023-08-27 16:47:47 -0500

    finished tests for #drop_piece

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -10,8 +10,29 @@ describe Grid do
     let(:symbol) { '⚫' }
 
     context 'when column is empty' do
+      let(:index) { 5 }
+
       it 'drops piece to bottom spot' do
-        expect { grid.drop_piece(column, symbol) }.to change { grid.spots[6][5] }.to(symbol)
+        expect { grid.drop_piece(column, symbol) }.to change { grid.spots[column][index] }.to(symbol)
+      end
+    end
+
+    context 'when same column has one piece' do
+      let(:next_index) { 4 }
+
+      it 'drops piece on top of last one' do
+        grid.drop_piece(column, symbol)
+        expect { grid.drop_piece(column, symbol) }.to change { grid.spots[column][next_index] }.to(symbol)
+      end
+    end
+
+    context 'when column is full' do
+      before do
+        6.times { grid.drop_piece(column, symbol) }
+      end
+
+      it 'returns :full' do
+        expect(grid.drop_piece(column, symbol)).to be :full
       end
     end
   end
