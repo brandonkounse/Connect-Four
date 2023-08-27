72d96f4125636b5bf85af57392248848b8f14a14
Brandon Kounse <kounse@gmail.com>
2023-08-27 11:51:13 -0500

    created singleton for outputting instructions

---
+++ lib/instructions.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Singleton for calling instructions for Connect Four
+class Instructions
+  def self.start
+    system 'clear'
+    puts <<~HEREDOC
+      In Connect Four, the game is won by getting 4 pieces in a row. The row can be horizontal, vertical, or diagonal. There are two colored pieces in this version, red and black. Good luck!
+    HEREDOC
+  end
+
+  def self.turn(player)
+    print "\n\nChoose a column to drop piece #{player.symbol} : "
+  end
+end
