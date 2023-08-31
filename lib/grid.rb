23810ccf986a20d9fe7584a536cb833b84f409f4
Brandon Kounse <kounse@gmail.com>
2023-08-31 09:43:37 -0500

    defined four_in_row method checks, wrote logic for four_in_column?

--- lib/grid.rb
+++ lib/grid.rb
@@ -11,22 +11,36 @@ class Grid
 
   def drop_piece(column, symbol, index = 5)
     if @spots[column][index].nil?
-      :full
+      false
     elsif @spots[column][index] == EMPTY_SPOT
       @spots[column][index] = symbol
+      true
     else
       drop_piece(column, symbol, index - 1)
     end
   end
 
   def render
-    row = format_grid
+    row = arrange
     puts "\n #{row.shift(@spots.length).join(' | ')}" until row.empty?
   end
 
+  def four_in_row?; end
+
+  def four_in_column?(column_index, symbol)
+    winning_indices = [[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5]]
+    winning_indices.any? do |indices|
+      @spots[column_index][indices.first, indices.length].all? { |element| element == symbol }
+    end
+  end
+
+  def four_in_diagonal; end
+
+  def four_in_reverse_diagonal; end
+
   private
 
-  def format
+  def arrange
     row = []
     index = 0
     row_length = 6
