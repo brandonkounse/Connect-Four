4c739cb15740ef99e2a0c4577cc7cafc8ee45ceb
Brandon Kounse <kounse@gmail.com>
2023-09-03 12:52:09 -0500

    refactored #check_sequence_of_four? and made adjustments to each method that calls it. wrote logic for returning diagonal indices

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,6 +3,7 @@
 # Gameplay area for Connect Four
 class Grid
   EMPTY_SPOT = '⚪'
+  SEQUENCE_LENGTH = 4
   attr_reader :spots
 
   def initialize
@@ -27,35 +28,41 @@ class Grid
   end
 
   def four_in_row?(row_index, symbol)
-    row_indices = [[0, 4], [1, 4], [2, 4], [3, 4]]
-    check_sequence_of_four?(indices: row_indices, grid: @spots.transpose, index: row_index, token: symbol)
+    check_sequence_of_four?(grid: @spots.transpose, index: row_index, token: symbol)
   end
 
   def four_in_column?(column_index, symbol)
-    column_indices = [[0, 4], [1, 4], [2, 4]]
-    check_sequence_of_four?(indices: column_indices, grid: @spots, index: column_index, token: symbol)
+    check_sequence_of_four?(grid: @spots, index: column_index, token: symbol)
   end
 
   def four_in_diagonal?(column_index, symbol)
-    diagonal_indices = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
-    row_index = @spots[column_index].count(EMPTY_SPOT)
-    diagonal_indices.any? do |direction|
-      mini = [[column_index, row_index]]
-      y = direction[0]
-      x = direction[1]
-      until mini.length == 4 || !(mini.last[0] + y).between?(0, 5) || !(mini.last[1] + x).between?(0, 6)
-        mini << [(mini.last[0] + y), (mini.last[1] + x)]
-      end
-      mini.map! { @spots[y][x] }
-      mini.all? { |element| element == symbol }
-    end
+    down_right = [[1, 1], [-1, -1]]
+    up_right = [[1, -1], [-1, 1]]
+    get_full_diagonal(column_index, down_right)
+    get_full_diagonal(column_index, up_right)
   end
 
   private
 
-  def check_sequence_of_four?(indices:, grid:, index:, token:)
-    indices.any? do |sequence|
-      grid[index][sequence.first, sequence.last].all? { |element| element == token }
+  def check_sequence_of_four?(grid:, index:, token:)
+    first_iteration = 0
+    max_iterations = grid[index].length % SEQUENCE_LENGTH
+    (first_iteration..max_iterations).any? do |iteration|
+      grid[index][iteration, SEQUENCE_LENGTH].all? { |element| element == token }
     end
+  end
+
+  def get_full_diagonal(column_index, directional_indices)
+    row_index = @spots[column_index].count(EMPTY_SPOT)
+    subset = [[column_index, row_index]]
+    directional_indices.each do |direction|
+      y = direction[0]
+      x = direction[1]
+      until !(subset.last[0] + y).between?(0, 6) || !(subset.last[1] + x).between?(0, 7)
+        next_spot = [(subset.last[0] + y), (subset.last[1] + x)]
+        subset << next_spot
+      end
+    end
+    subset.uniq.sort.map { |x, y| @spots[x][y] }
   end
 end
